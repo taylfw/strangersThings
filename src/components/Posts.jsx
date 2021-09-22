0bb9922e1f2b877fb1839b8f3f547b849fff5a18
taylfw88 <frank.w.taylor88@gmail.com>
2021-09-22 06:11:33 -0400

    I got the posts to show up in console

---
+++ src/components/Posts.jsx
@@ -0,0 +1,11 @@
+import { getPosts } from "../api";
+
+const Posts = () => {
+  return (
+    <div>
+      <h1>This is the Posts component.</h1>
+    </div>
+  );
+};
+
+export default Posts;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import Posts from "./components/Posts";
 
 import {
   BrowserRouter as Router,
@@ -7,14 +8,39 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
-
+import { getPosts } from "./api";
 import { Header } from "./components";
+import axios from "axios";
 
 const App = () => {
+  //I was meaning to change these variables for less confusion (also reflected on line 23 through 25 👇️ )
+  const [allPosts, setAllPosts] = useState([]);
+
+  const fetchAllPosts = async () => {
+    try {
+      const response = await axios.get(
+        "https://strangers-things.herokuapp.com/api/2106-UNF-RM-WEB-PT/posts"
+      );
+
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllPosts();
+  }, []);
+
   return (
     <Router>
       <div id="App">
         <Header />
+        <Posts
+          allPosts={allPosts}
+          setAllPosts={setAllPosts}
+          getPosts={getPosts}
+        />
       </div>
     </Router>
   );
