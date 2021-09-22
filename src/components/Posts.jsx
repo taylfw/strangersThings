dd2ada73c677935b5bb9d25c54e0ee26b14ec46c
taylfw88 <frank.w.taylor88@gmail.com>
2021-09-22 19:48:48 -0400

    Made post stylish!

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,21 +1,19 @@
 import { getPosts } from "../api";
-
+import "./Post.css";
 const Posts = (props) => {
   const { allPosts } = props;
 
   return (
     <div>
-      <div>
-        {allPosts
-          ? allPosts.map((post) => {
-              return (
-                <div key={post._id}>
-                  title: {post.title} {post.description} {post.price}
-                </div>
-              );
-            })
-          : null}
-      </div>
+      {allPosts
+        ? allPosts.map((post) => {
+            return (
+              <div key={post._id} className="card">
+                title: {post.title} {post.description} {post.price}
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 };
