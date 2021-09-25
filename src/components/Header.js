c3572008d459017af57b426a0b1130db4abc83cd
thatsme <ambergray930@yahoo.com>
2021-09-25 09:56:24 -0400

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <h1>Header Component</h1>
+      <h1 class="titleStyles">strangers </h1>
+      <h2 class="titleStyles">Things</h2>
     </header>
   );
 };
